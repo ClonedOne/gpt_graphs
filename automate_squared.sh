86679baf9b2a664759974e88220022d52dea2a93
ClonedOne <ClonedOne@users.noreply.github.com>
2023-02-25 18:45:38 -0500

    Updates to notebook crossing

--- automate_squared.sh
+++ automate_squared.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Run command
-python gui_query_gpt.py --query_dir queries/transpose_prompts_no_steps_median_continuation
+python gui_query_gpt.py --query_dir queries/longer_edge_length_prompts_icl
 
 # sleep for 5 seconds
 sleep 5
 
 # Run command
-python gui_query_gpt.py --query_dir queries/transpose_prompts_steps_median_continuation
+python gui_query_gpt.py --query_dir queries/crossings_prompts_icl
 
 # sleep for 5 seconds
 sleep 5
