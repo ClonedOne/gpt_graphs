71c9a31c74293affd3bbf1dcebd192b3fe92b56a
ClonedOne <ClonedOne@users.noreply.github.com>
2023-02-25 14:33:26 -0500

    Latest answers

--- automate_squared.sh
+++ automate_squared.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Run command
-python gui_query_gpt.py --query_dir queries/rank_prompts_icl
+python gui_query_gpt.py --query_dir queries/transpose_prompts_no_steps_median_continuation
 
 # sleep for 5 seconds
 sleep 5
 
 # Run command
-python gui_query_gpt.py --query_dir queries/crossings_prompts
+python gui_query_gpt.py --query_dir queries/transpose_prompts_steps_median_continuation
 
 # sleep for 5 seconds
 sleep 5
 
 # Run command
-python gui_query_gpt.py --query_dir queries/transpose_prompts_icl
+# python gui_query_gpt.py --query_dir queries/crossings_prompts_steps
